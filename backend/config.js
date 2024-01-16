0ba267d251cd7540e454b8302766a20bbea6c88f
Sachin kumar <110657130+sachinDev07@users.noreply.github.com>
2024-01-16 12:31:38 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export const PORT = 3000;
 
 export const mongoDBURL =
-  "mongodb+srv://sachinkrj077:bookstore990@book-store-mern.gqj1znr.mongodb.net/books-collection?retryWrites=true&w=majority";
+  "your_mongodb_url";
